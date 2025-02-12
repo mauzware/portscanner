91a34682646536f4e3a6b6ccdb6259441501234c
m4uzw4r3 <mauzware@gmail.com>
2025-02-12 12:36:02 +0100

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,35 @@
+import socket
+import termcolor
+
+
+
+def scan(target, ports):
+	print('\n' + ' Starting Scan For ' + str(target))
+	for port in range(1, ports):
+		scan_port(target, port)
+
+
+def scan_port(ipaddress, port):
+	try:
+		sock = socket.socket()
+		sock.connect((ipaddress, port))
+		try:
+			banner = sock.recv(1024).decode()
+			print("[+] Port {} is open with banner {}".format(port, banner))
+			sock.close()
+		except:
+			print("[+] Port Opened " + str(port))
+			sock.close()
+	except:
+		pass
+
+
+targets = input("[*] Enter Targets to Scan(split them by ,): ")
+ports = int(input("[*] Enter how many Ports do You want to Scan: "))
+if ',' in targets:
+	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+	for ip_addr in targets.split(','):
+		scan(ip_addr.strip(' '), ports)
+else:
+	scan(targets, ports)
+
